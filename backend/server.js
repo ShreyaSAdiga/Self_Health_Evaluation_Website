ce0ff21a40aafae879cf256eb08fd08749cac36d
ShreyaSAdiga <80194836+ShreyaSAdiga@users.noreply.github.com>
2021-12-12 09:10:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ const connection=mongoose.connection;
 connection.once('open',()=>{console.log("MongoDB database connection established");})
 
 const detailsRouter=require('./routes/details')
-const usersRouter=require('./routes/users');
+//const usersRouter=require('./routes/users');
 
 app.use('/details',detailsRouter);
-app.use('/users',usersRouter);
+//app.use('/users',usersRouter);
 
 app.listen(port,()=>{
     console.log(`Server running on port: ${port}`);
