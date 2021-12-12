e213091cc62117363c34e9c5407467a06535b862
ShreyaSAdiga <80194836+ShreyaSAdiga@users.noreply.github.com>
2021-12-12 09:50:09 +0530

    Update detail.model.js

--- backend/models/detail.model.js
+++ backend/models/detail.model.js
@@ -1,13 +1,22 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs');
 const Schema=mongoose.Schema;
 const detailSchema= new Schema({
     username:{type:String,required: true},
+    password:{type:String,required: true},
+    password_c:{type:String,required:true},
     emailId:{type:String,required:true},
     contactno:{type:Number,required:true},
     address:{type:String,required:true}
-},
-{
-    timestamps: true,
+});
+
+
+detailSchema.pre('save',async function(next){
+    if(this.isModified('password')){
+        this.password=await bcrypt.hash(this.password,12);        
+        this.password_c=await bcrypt.hash(this.password_c,12);
+    }
+    next();
 });
 
 const Detail=mongoose.model('Detail',detailSchema);
